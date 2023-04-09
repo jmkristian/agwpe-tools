58c6584943e99130b4a6612790b6d751059d6949
John Kristian <jmkristian@gmail.com>
2023-04-08 17:38:51 -0700

    Don't exit build.sh because converse.exe doesn't exist.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname "$0"` || exit $?
-test -e converse.exe && rm converse.exe || exit $?
+test -e converse.exe && rm converse.exe
 node_modules/.bin/pkg.cmd -t node12-win-x86 converse.js || exit $?
 ./sign.cmd converse.exe ..\\codeSigning\\signingCert.pfx || exit $?
