ac1c2976c58033815d41f5344afa7f02f175b657
John Kristian <jmkristian@gmail.com>
2024-05-25 09:41:04 -0700

    Fix bugs in class Lines, and change its API.

--- lines.js
+++ lines.js
@@ -4,96 +4,93 @@ const OS = require('os');
 const shared = require('./shared.js');
 const Stream = require('stream');
 
-const localEncoding = 'utf8';
-const remoteEncoding = 'binary'; // TODO
-
 class Lines extends Stream.Writable {
 
-    constructor(escape, logger) {
+    constructor(stdout, escape, log) {
         super({
             decodeStrings: false, // Don't convert strings to bytes
-            encoding: remoteEncoding,
+            encoding: 'binary',
             highWaterMark: 128,
         });
+        this.stdout = stdout;
         this.ESC = escape;
-        this.INT = shared.INT;
-        this.TERM = shared.TERM;
-        if (this.INT || this.TERM) {
-            this.pattern = new RegExp(`${this.INT}|${this.TERM}`, 'g');
-        }
-        this.log = logger || shared.LogNothing;
+        this.log = log || shared.LogNothing;
         this.buffer = '';
         this.prefix = '';
+        this.INT = shared.INT;
+        this.TERM = shared.TERM;
+        const specials = [this.INT, this.TERM].filter(function(c) {return c;});
+        if (specials.length > 0) {
+            this.pattern = new RegExp(specials.join('|'), 'g');
+        }
         const that = this;
         this.on('pipe', function(from) {
             that.isRaw = false;
             try {
-                if (from.isTTY) {
-                    if (from.setEncoding) from.setEncoding(localEncoding);
-                    if (from.setRawMode) {
-                        from.setRawMode(true);
-                        that.isRaw = true;
-                    }
+                if (from.isTTY && from.setRawMode) {
+                    from.setRawMode(true);
+                    that.isRaw = true;
                 }
             } catch(err) {
                 that.log.error(err);
             }
         });
-        process.stdin.pipe(this);
     }
 
-    prompt(prefix) {
-        if (this.prefix != prefix) {
-            var data = '';
-            if (this.prefix) {
-                for (var p = 0; p < this.prefix.length; ++p) {
-                    data += '\b \b';
-                }
+    prompt(prefix, callback) {
+        if (this.prefix == prefix) {
+            if (callback) callback();
+        } else {
+            var output = '';
+            for (var b = this.prefix.length + this.buffer.length; b > 0 ; --b) {
+                output += '\b \b';
             }
-            for (var b = 0; b < this.buffer.length; ++b) {
-                data += '\b';
-            }
-            this.prefix = prefix;
-            data += this.prefix + this.buffer;
-            process.stdout.write(data);
+            this.prefix = '' + prefix;
+            output += this.prefix + this.buffer;
+            this.stdout.write(output, callback);
         }
     }
 
-    _clearLine() {
-        var data = '';
-        for (var d = this.prefix.length + this.buffer.length; d > 0; --d) {
-            data += '\b \b';
+    clearBuffer() {
+        var output = '';
+        for (var b = this.prefix.length + this.buffer.length; b > 0; --b) {
+            output += '\b \b';
         }
-        process.stdout.write(data);
+        this.stdout.write(output);
         this.prefix = '';
         this.buffer = '';
     }
 
-    _emitLine() {
+    emitBuffer() {
         const line = this.buffer;
-        this._clearLine();
+        this.clearBuffer();
         this.emit('line', line);
     }
 
+    /** Show a line to the user. */
     writeLine(chunk, encoding, callback) {
-        const line = Buffer.isBuffer(chunk) ? chunk.toString(encoding)
-              : (typeof chunk) == 'string' ? chunk
-              : '' + chunk;
-        var extra = line.length - (this.prefix.length + this.buffer.length);
+        const line = (typeof chunk) == 'string' ? chunk : chunk.toString();
         var output = '';
-        for (var e = 0; e < extra; ++e) {
+        var b = this.prefix.length + this.buffer.length;
+        // Erase part of the buffered line:
+        for (; b > line.length; --b) {
             output += '\b \b';
         }
-        for (var c = 0; c < line.length; ++c) {
+        // Move the cursor all the way to the left:
+        for (; b > 0; --b) {
             output += '\b';
         }
-        output += line + OS.EOL + this.prefix + this.buffer;
-        process.stdout.write(output, callback);
+        output += line // Over-write the buffered line with the new line,
+            + OS.EOL + this.prefix + this.buffer; // and restore the buffered line.
+        this.stdout.write(output, callback);
     }
 
+    /** Handle input coming from the user. */
     _write(chunk, encoding, callback) {
         shared.logChunk(this.log, this.log.trace, 'Lines._write(%s)', chunk, encoding);
-        var data = Buffer.isBuffer(chunk) ? chunk.toString(localEncoding) : chunk;
+        var data = (typeof chunk) == 'string' ? chunk
+            : chunk == null ? '' // That's weird.
+            : chunk.toString();
         if (this.pattern) {
             const that = this;
             data = data.replace(this.pattern, function(found) {
@@ -102,35 +99,34 @@ class Lines extends Stream.Writable {
                 return '';
             });
         }
+        var output = '';
         for (var d = 0; d < data.length; ++d) {
-            var c = data.substring(d, d + 1);
+            var c = data.charAt(d);
             switch(c) {
             case shared.BS:
             case shared.DEL:
-                if (this.buffer.length) {
-                    process.stdout.write('\b \b');
+                if (this.buffer.length > 0) {
+                    output += '\b \b';
                     this.buffer = this.buffer.substring(0, this.buffer.length - 1);
                 }
                 break;
-            case '\x15': // erase buffer
-                var data = '';
-                for (var d = this.buffer.length; d > 0; --d) {
-                    data += '\b \b';
+            case '\x15': // Ctrl+U erase buffer
+                for (var b = this.buffer.length; b > 0; --b) {
+                    output += '\b \b';
                 }
-                process.stdout.write(data);
                 this.buffer = '';
                 break;
             case '\n':
             case '\r':
-                this._emitLine();
+                this.emitBuffer();
                 break;
             case this.ESC:
-                this._clearLine();
+                this.clearBuffer();
                 this.emit('escape');
                 break;
             case shared.EOT:
                 if (this.buffer) {
-                    this._emitLine();
+                    this.emitBuffer();
                 } else {
                     this.emit('close');
                 }
@@ -140,10 +136,10 @@ class Lines extends Stream.Writable {
                 break;
             default:
                 this.buffer += c;
-                process.stdout.write(c);
+                output += c;
             }
         }
-        if (callback) callback();
+        this.stdout.write(output, callback);
     }
 
 } // Lines
