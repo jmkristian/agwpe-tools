ddd2c71fdc2b8dd981cf239e8c5c1ed64631741f
John Kristian <jmkristian@gmail.com>
2024-05-27 16:31:47 -0700

    Build chatter.exe (in addition to converse.exe).

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cd `dirname "$0"` || exit $?
+
+test -e chatter.exe && rm chatter.exe
+node_modules/.bin/pkg.cmd -t node12-win-x86 chatter.js || exit $?
+./sign.cmd chatter.exe ..\\codeSigning\\signingCert.pfx || exit $?
+
 test -e converse.exe && rm converse.exe
 node_modules/.bin/pkg.cmd -t node12-win-x86 converse.js || exit $?
 ./sign.cmd converse.exe ..\\codeSigning\\signingCert.pfx || exit $?
