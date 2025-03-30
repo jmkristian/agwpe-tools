ace66e2678402f2a2db6502513740073c817c62c
John Kristian <jmkristian@gmail.com>
2025-03-29 18:38:57 -0700

    Echo the Windows commands that build chatter.exe and converse.exe.

--- build.cmd
+++ build.cmd
@@ -1,13 +1,13 @@
 @pushd "%~dp0"
 
 @if exist chatter.exe del chatter.exe
-@call node_modules\.bin\pkg.cmd -t node12-win-x86 chatter.js
+call node_modules\.bin\pkg.cmd -t node12-win-x86 chatter.js
 @if %ERRORLEVEL% neq 0 goto :err
 @call .\sign.cmd chatter.exe ..\codeSigning\signingCert.pfx
 @if %ERRORLEVEL% neq 0 goto :err
 
 @if exist converse.exe del converse.exe
-@call node_modules\.bin\pkg.cmd -t node12-win-x86 converse.js
+call node_modules\.bin\pkg.cmd -t node12-win-x86 converse.js
 @if %ERRORLEVEL% neq 0 goto :err
 @call .\sign.cmd converse.exe ..\codeSigning\signingCert.pfx
 @if %ERRORLEVEL% neq 0 goto :err
