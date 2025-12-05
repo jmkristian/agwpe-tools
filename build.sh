a9ff5d41de2b1d09cefd74a0a7b5a59ddb189739
John Kristian <jmkristian@gmail.com>
2025-12-05 11:38:23 -0600

    Use @yao-pkg/pkg to build executable files for Raspberry Pi armv7.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd `dirname "$0"` || exit $?
 
 test -e chatter && rm chatter
-node_modules/.bin/pkg -t node12-linux chatter.js || exit $?
+node_modules/.bin/pkg -t node14-linuxstatic chatter.js || exit $?
 
 test -e converse && rm converse
-node_modules/.bin/pkg -t node12-linux converse.js || exit $?
+node_modules/.bin/pkg -t node14-linuxstatic converse.js || exit $?
