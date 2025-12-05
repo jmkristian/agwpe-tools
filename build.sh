6b33cdf0ae03c442e9cb9f04b619c36a231532eb
John Kristian <jmkristian@gmail.com>
2025-12-05 13:32:52 -0600

    Build executable files for the armv7 architecure, explicitly.

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 cd `dirname "$0"` || exit $?
 
 test -e chatter && rm chatter
-node_modules/.bin/pkg -t node14-linuxstatic chatter.js || exit $?
+node_modules/.bin/pkg -t node14-linuxstatic-armv7 chatter.js || exit $?
 
 test -e converse && rm converse
-node_modules/.bin/pkg -t node14-linuxstatic converse.js || exit $?
+node_modules/.bin/pkg -t node14-linuxstatic-armv7 converse.js || exit $?
